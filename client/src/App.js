81ac7307f7ce4c8736f35a8ec05d4de33190c925
DmalTzi-WSL <dmaltzi@DESKTOP-955KS46>
2023-09-08 00:04:35 +0700

    init2

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import NavbarComponent from "./components/NavbarComponent";
 import axios from "axios"
-import {usestate,useEffect} from "react"
+import {useState,useEffect} from "react"
 
 function App() {
 
-  const [blogs, setBlogs] = usestate([])
+  const [blogs, setBlogs] = useState([])
+
+  const fetchData=async()=>{
+    await axios
+      .get(`${process.env.REACT_APP_API}/blogs`)
+      .then(respone=>{
+        setBlogs(respone.data)
+      })
+      .catch(err=>alert(err))
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
 
   return (
     <div className="container p-5">
       <NavbarComponent/>
-      
+      {JSON.stringify(blogs)}
     </div>
   );
 }
